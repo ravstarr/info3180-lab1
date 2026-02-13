5a765294b5370494971e5107ed188d1d63279d8e
Ravaughn Marsh <ravaughnmarsh@Ravs-MacBook-Air.local>
2026-02-13 07:47:56 -0500

    Completed Lab 1

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+'''
+# Routing for your application.
+# Put your routes below this comment
+'''
+
+@app.route('/')
+def home():
+    return 'My home page'
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
